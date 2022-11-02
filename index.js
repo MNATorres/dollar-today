164ef5c0088f4d61e01f88e8626c217f1c4843e7
Federico Jure <fjure@atixlabs.com>
2022-11-02 00:07:35 -0300

    add google analitics

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-$( document ).ready(function() {
+$(document).ready(function () {
     $("#updated").text(document.lastModified)
     $.ajax({
         dataType: "json",
         url: "/values.json",
-        success: (data) => {
+        success: (data, _, xhr) => {
+            $("#updated").text(new Date(xhr.getResponseHeader('Last-Modified')).toLocaleString())
             Object.keys(data).forEach(key => {
+
                 const item = $(`[name="${key}"]`)
                 item.find('#compra').text(getValueText(data[key].compra))
                 item.find('#venta').text(getValueText(data[key].venta))
             })
         }
-      });
+    });
 });
 
 function getValueText(text) {
